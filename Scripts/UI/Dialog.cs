89136ffa1a998a03e4c5f1229f88a6a440a11f39
Andreas Bresser <self@andreasbresser.de>
2017-10-05 14:29:00 +0200

    Basic Dialogs

---
+++ Scripts/UI/Dialog.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MRUI
+{
+    [ExecuteInEditMode]
+    public class Dialog : MonoBehaviour
+    {
+        public string title;
+        public DialogTitle dialogTitle;
+        public ButtonGroup buttonGroup;
+
+        public void OnEnable()
+        {
+            dialogTitle = dialogTitle ?? GetComponentInChildren<DialogTitle>();
+            buttonGroup = buttonGroup ?? GetComponentInChildren<ButtonGroup>();
+            if (buttonGroup != null)
+            {
+                buttonGroup.OnDataChange.AddListener(OnDataChange);
+            }
+        }
+
+        public void OnDataChange()
+        {
+            if (buttonGroup != null)
+            {
+                int columns = (buttonGroup.data.Count) / buttonGroup.maxRows;
+                if (buttonGroup.data.Count % buttonGroup.maxRows > 0)
+                {
+                    columns++;
+                }
+                float scaleX = buttonGroup.transform.localScale.x * buttonGroup.buttonDistanceHorizontal;
+                dialogTitle.SetColumns(columns, scaleX);
+            }
+        }
+
+        public void OnValidate()
+        {
+            if (dialogTitle != null && dialogTitle.text != null)
+            {
+                dialogTitle.text.text = title;
+            }
+        }
+
+        public void OnDisable()
+        {
+            buttonGroup.OnDataChange.RemoveListener(OnDataChange);
+        }
+    }
+}
---
+++ Scripts/UI/DialogTitle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MRUI
+{
+    [ExecuteInEditMode]
+    public class DialogTitle : MonoBehaviour
+    {
+        public GameObject background;
+        public Text text;
+        public RectTransform rectTransform;
+
+        [Tooltip("height of the text canvas")]
+        public float textHeight = 120f;
+
+        [Tooltip("width of the text canvas")]
+        public float columnWidth = 420f;
+
+        [Tooltip("Border between text Canvas and Text ui component")]
+        public float border = 8f;
+
+        private int columns = 0;
+        private float scaleX = 0;
+
+        public void OnEnable()
+        {
+            text = text ?? GetComponentInChildren<Text>();
+            rectTransform = rectTransform ?? GetComponentInChildren<RectTransform>();
+            if (background == null)
+            {
+                foreach (Transform child in transform)
+                {
+                    if (child.gameObject.name == "Background")
+                    {
+                        background = child.gameObject;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void OnValidate()
+        {
+            if (columns > 0 && scaleX > 0)
+            {
+                SetColumns(this.columns, this.scaleX);
+            }
+        }
+
+        public void SetColumns(int columns, float scaleX)
+        {
+            this.columns = columns;
+            this.scaleX = scaleX;
+            Vector3 scale = background.transform.localScale;
+            scale.x = columns * scaleX;
+            background.transform.localScale = scale;
+
+            text.rectTransform.sizeDelta = new Vector2(columnWidth * columns - border, textHeight - border);
+            rectTransform.sizeDelta = new Vector2(columnWidth * columns, textHeight);
+        }
+    }
+}
